57122c3bcb5cefef21ed36815e915f7083ae3bde
83315 <alshaibaninada@gmail.com>
2024-04-14 10:53:06 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = "Nada";
+int age = 23;
+double gpa = 3.71;
+bool graduate = true;
+int number = 45; 
+
+Console.WriteLine(name);
+Console.WriteLine(age);
+Console.WriteLine(gpa);
+Console.WriteLine(graduate);
+Console.WriteLine($"{number:0.00}");
+Console.WriteLine("-----------");
+
+
+// task 2
+
+var studentName = "Aseel";
+var grade = "A";
+var favouriteSubject = "Math";
+var height = 3.2;
+var width = 5.5;
+var area = height * width;
+
+
+Console.WriteLine(studentName);
+Console.WriteLine(grade);
+Console.WriteLine(favouriteSubject);
+Console.WriteLine("Area of a Rectangle: " +area);
